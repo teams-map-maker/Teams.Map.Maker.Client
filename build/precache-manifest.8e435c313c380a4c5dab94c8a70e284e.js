bf7987fe363aca1afa593cb29adbc22426cf99d2
Johannes Wesslen Weiler <jowessle@microsoft.com>
2021-01-27 15:26:42 +0100

    updated package.json

---
+++ build/precache-manifest.8e435c313c380a4c5dab94c8a70e284e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8070699e8574aad0e3dd5079682db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea18f473fe22df26b53",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3dba8dbc3b361c106949",
+    "url": "/static/css/main.4b42cd75.chunk.css"
+  },
+  {
+    "revision": "9ea18f473fe22df26b53",
+    "url": "/static/js/2.96d3da92.chunk.js"
+  },
+  {
+    "revision": "3dba8dbc3b361c106949",
+    "url": "/static/js/main.052882cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
